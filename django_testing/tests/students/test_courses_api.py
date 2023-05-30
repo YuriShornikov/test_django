855c05ef76c623d5b978ba5a1826fcad5351d2e5
Yuri Shornikov <dbonlyfun@gmail.com>
2023-05-30 23:42:34 +0300

    t

--- django_testing/tests/students/test_courses_api.py
+++ django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,35 @@
-def test_example():
-    assert False, "Just test example"
+from model_bakery import baker
+from rest_framework.test import APIClient
+import pytest
+
+from students.models import Course, Student
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+@pytest.fixture
+def course():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+
+    return factory
+
+@pytest.fixture
+def student():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+
+    return factory
+
+def test_get_courses(client, course):
+    courses = course(_quantity=10)
+
+    response = client.get('/courses/')
+    assert response.status_code == 200
+
+
+# def test_example():
+#
+#     assert False, "Just test example"
